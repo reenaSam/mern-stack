ee639f6474df64ea47f76fd4e1f91f2fee6cf64d
reenaSam <reenanewage123@gmail.com>
2023-10-12 14:20:03 +0530

    create user update api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
 
 dotenv.config();
 
@@ -19,6 +21,9 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser())
+
 
 
 
