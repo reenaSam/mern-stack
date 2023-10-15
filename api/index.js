3a9c1b5dfac745bc2c2d49281e3c67c3938716c5
reenaSam <reenanewage123@gmail.com>
2023-10-15 20:06:14 +0530

    add  pre installation for deploying

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 
 dotenv.config();
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const _dirname = path.resolve();
 
 
 const app = express();
@@ -34,7 +37,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/listing',listingRouter)
+app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(_dirname, 'client','dist','index.html'))
+})
 
 
 app.use((err, req, res, next) => {
